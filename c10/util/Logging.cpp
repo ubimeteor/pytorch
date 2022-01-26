0d8eb7dfa63ae43562eb4983948926d2d2ace96f
Can Balioglu <cbalioglu@users.noreply.github.com>
2022-01-26 11:52:05 -0800

    Introduce an environment variable to change c10 log level
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/71746
    
    This PR contains the following improvements:
    
    - It exposes a new environment variable `TORCH_CPP_MIN_LOG_LEVEL` that enables users to set the log level of c10 logging facility (supports both GLOG and c10 loggers). Valid values are `INFO`, `WARNING`, `ERROR`, and `FATAL` or their numerical equivalents `0`, `1`, `2`, and `3`.
    - It implements an `initLogging()` function and calls it as part of `torch._C` module import to ensure that the underlying logging facility is correctly initialized in Python.
    
    With these changes a user can dynamically set the log level of c10 as in the following example:
    
    ```
    $ TORCH_CPP_MIN_LOG_LEVEL=INFO python my_torch_script.py
    ```
    ghstack-source-id: 147718690
    
    Differential Revision: [D33756252](https://our.internmc.facebook.com/intern/diff/D33756252/)

--- c10/util/Logging.cpp
+++ c10/util/Logging.cpp
@@ -9,6 +9,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <mutex>
 #include <numeric>
 
 // Common code that we use regardless of whether we use glog or not.
@@ -145,8 +146,13 @@ bool LogAPIUsageFakeReturn(const std::string& event) try {
   // static destructor race
   return true;
 }
-} // namespace detail
 
+namespace {
+
+void setLogLevelFlagFromEnv();
+
+} // namespace
+} // namespace detail
 } // namespace c10
 
 #if defined(C10_USE_GFLAGS) && defined(C10_USE_GLOG)
@@ -198,15 +204,15 @@ bool IsGoogleLoggingInitialized();
 } // namespace google
 
 namespace c10 {
-bool InitCaffeLogging(int* argc, char** argv) {
-  if (*argc == 0)
-    return true;
+namespace {
+
+void initGoogleLogging(char const* name) {
 #if !defined(_MSC_VER)
   // This trick can only be used on UNIX platforms
   if (!::google::glog_internal_namespace_::IsGoogleLoggingInitialized())
 #endif
   {
-    ::google::InitGoogleLogging(argv[0]);
+    ::google::InitGoogleLogging(name);
 #if !defined(_MSC_VER)
     // This is never defined on Windows
 #if !defined(__XROS__)
@@ -214,7 +220,30 @@ bool InitCaffeLogging(int* argc, char** argv) {
 #endif
 #endif
   }
+}
+
+} // namespace
+
+void initLogging() {
+  // Unlike caffe2 torch always writes to stderr.
+  FLAGS_logtostderr = 1;
+
+  initGoogleLogging("torch-cpp");
+
+  detail::setLogLevelFlagFromEnv();
+
   UpdateLoggingLevelsFromFlags();
+}
+
+bool InitCaffeLogging(int* argc, char** argv) {
+  if (*argc == 0) {
+    return true;
+  }
+
+  initGoogleLogging(argv[0]);
+
+  UpdateLoggingLevelsFromFlags();
+
   return true;
 }
 
@@ -254,6 +283,11 @@ C10_DEFINE_int(
     "The minimum log level that caffe2 will output.");
 
 namespace c10 {
+
+void initLogging() {
+  detail::setLogLevelFlagFromEnv();
+}
+
 bool InitCaffeLogging(int* argc, char** argv) {
   // When doing InitCaffeLogging, we will assume that caffe's flag parser has
   // already finished.
@@ -356,3 +390,56 @@ MessageLogger::~MessageLogger() {
 } // namespace c10
 
 #endif // !C10_USE_GLOG
+
+namespace c10 {
+namespace detail {
+namespace {
+
+void unsafeSetLogLevelFlagFromEnv() {
+  const char* level_str = std::getenv("TORCH_CPP_MIN_LOG_LEVEL");
+
+  // Not set, fallback to the default level (i.e. WARNING).
+  if (level_str == nullptr) {
+    return;
+  }
+
+  std::string level{level_str};
+  if (level.empty()) {
+    return;
+  }
+
+  if (level == "0" || level == "INFO" || level == "info") {
+    FLAGS_caffe2_log_level = 0;
+
+    return;
+  }
+  if (level == "1" || level == "WARNING" || level == "warning") {
+    FLAGS_caffe2_log_level = 1;
+
+    return;
+  }
+  if (level == "2" || level == "ERROR" || level == "error") {
+    FLAGS_caffe2_log_level = 2;
+
+    return;
+  }
+  if (level == "3" || level == "FATAL" || level == "fatal") {
+    FLAGS_caffe2_log_level = 3;
+
+    return;
+  }
+
+  std::cerr << "`TORCH_CPP_MIN_LOG_LEVEL` environment variable cannot be parsed. Valid values are "
+               "`INFO`, `WARNING`, `ERROR`, and `FATAL` or their numerical equivalents `0`, `1`, "
+               "`2`, and `3`." << std::endl;
+}
+
+void setLogLevelFlagFromEnv() {
+  static std::once_flag f{};
+
+  std::call_once(f, unsafeSetLogLevelFlagFromEnv);
+}
+
+} // namespace
+} // namespace detail
+} // namespace c10
